a671b43df84108dda1d58d80880fee8eefd90fae
rweston03 <randi.l.weston@gmail.com>
2021-07-23 19:43:11 -0600

    trying to get heroku routes to work

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 
 const mongoose = require('mongoose');
 
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+    // add other server routes to path array
+    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
+} 
+
 require('dotenv').config();
 
 const app = express();
